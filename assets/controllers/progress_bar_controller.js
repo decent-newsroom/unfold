f0b3a39a3bfce85fd3807834b92862d8c77d9764
Nuša Pukšič <nusa.puksic@gmail.com>
2025-06-08 11:36:25 +0200

    Progress bar

---
+++ assets/controllers/progress_bar_controller.js
@@ -0,0 +1,33 @@
+// assets/controllers/progress_bar_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["bar"];
+
+  connect() {
+    // Listen for clicks on the entire document instead of just the controller element
+    document.addEventListener("click", this.handleClick.bind(this));
+  }
+
+  disconnect() {
+    // Clean up event listener when controller disconnects
+    document.removeEventListener("click", this.handleClick.bind(this));
+  }
+
+  handleClick(event) {
+    const link = event.target.closest("a");
+    if (link && !link.hasAttribute("data-no-progress") &&
+        !event.ctrlKey && !event.metaKey && !event.shiftKey) {
+      this.start();
+    }
+  }
+
+  start() {
+    this.barTarget.style.width = "0";
+    this.barTarget.style.transition = "none";
+    setTimeout(() => {
+      this.barTarget.style.transition = "width 5s ease-in-out";
+      this.barTarget.style.width = "100%";
+    }, 10);
+  }
+}
