c38a58a5e55081a4710bf391a202231962e177a1
Nuša Pukšič <nusa.puksic@gmail.com>
2025-06-17 19:41:58 +0200

    Try to make cron work

--- docker/cron/index_articles.sh
+++ docker/cron/index_articles.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
+export PATH="/usr/local/bin:/usr/bin:/bin"
 
 # Run Symfony commands sequentially
-/usr/local/bin/php /var/www/html/bin/console articles:get
-/usr/local/bin/php /var/www/html/bin/console articles:qa
-/usr/local/bin/php /var/www/html/bin/console articles:index
-/usr/local/bin/php /var/www/html/bin/console articles:indexed
+php /var/www/html/bin/console articles:get -- '-1 week' 'now'
+php /var/www/html/bin/console articles:qa
+php /var/www/html/bin/console articles:index
+php /var/www/html/bin/console articles:indexed
